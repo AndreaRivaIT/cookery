595c0d660608243028717839dd6cf44038bebdd6
Alessandro Cattaneo <a.cattaneo81@campus.unimib.it>
2021-11-21 16:44:05 +0100

    aggiunta scelta filtri e finito chip

--- app/src/main/java/it/unimib/cookery/MyRecipesFragment.java
+++ app/src/main/java/it/unimib/cookery/MyRecipesFragment.java
@@ -1,13 +1,15 @@
 package it.unimib.cookery;
 
 import android.os.Bundle;
-
+import android.content.DialogInterface;
+import java.util.Collections;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import androidx.appcompat.app.AlertDialog;
 import android.util.Log;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Filter;
 import android.widget.GridView;
 import android.view.LayoutInflater;
@@ -15,6 +17,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 
+
+import com.google.android.material.chip.Chip;
 
 import java.util.ArrayList;
 
@@ -38,9 +42,17 @@ public class MyRecipesFragment extends Fragment {
     /* dichiaro un oggetto di tipo SearchView */
     private SearchView recipiesSearch;
 
+    /* dichiaro un oggetto di tipo chip*/
+    private Chip chip;
+
+    /* dichiaro un oggetto di tipo textButton*/
+    private Button buttonFilter;
 
     /* crea l'array list di elementi da mostrare nell gridView */
     ArrayList<Recipe> recipeArrayList = new ArrayList<Recipe>();
+
+    /* array list per i filtri */
+    ArrayList<Integer> filterList = new ArrayList<>();
 
 
 
@@ -116,6 +128,108 @@ public class MyRecipesFragment extends Fragment {
         });
 
         //-- fine codice searchView--
+
+
+       // -- inizio codice chip --
+
+        /* ottengo l'elemento chip */
+       chip = view.findViewById(R.id.addChip);
+
+       /* listener dell'oggetto chip*/
+
+        chip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("premuto", "premuto chip add");
+            }
+        });
+
+       // -- fine codice chip --
+
+
+
+        // --inizio codice button filter --
+
+        // array di nomi che verranno mostrati sulla dialog
+        String[] filterArray = {"stuzzichini","primi","secondi","contorni","dolci"};
+
+        // serve per salvare se un elemento è selezionato o meno
+        boolean[] selectedFilter = new boolean[filterArray.length];
+
+
+        /* ottengo l'oggetto button */
+        buttonFilter = view.findViewById(R.id.buttonFilter);
+
+        /* listener dell'oggetto buttonFilter */
+
+        buttonFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("premuto", "premuto button filter");
+
+                // inizializza l'alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                // setta il titolo
+                builder.setTitle("Select recipe type");
+
+                // messo a false non permette di uscire dalla dialog se non premendo sul tasto close
+                builder.setCancelable(false);
+
+                // creo il menu a scelta multipla
+                builder.setMultiChoiceItems(filterArray, selectedFilter, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        // check condition
+                        if (b) {
+                            // quando la checkbox è selezionata
+                            // creo l'elemento da aggiungere
+                            Integer element = i;
+                            // lo aggiungo
+                            filterList.add(element);
+
+                        } else {
+                            // quando deseleziono la checkbox
+                            // creo l'elemento da rimuovere
+                            Integer element = i;
+                           // trovo il suo indice nell'array list
+                            int index = filterList.indexOf(element);
+                            // lo rimuovo dalla lista
+                            filterList.remove(index);
+
+                        }
+                    }
+                });
+
+                // crea bottone ok
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                            // se premuto ok far partire la ricerca
+                    }
+
+                });
+
+                // crea bottone close
+                builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // se premuto colose chiude l'alert dialog
+                        dialogInterface.dismiss();
+                    }
+                });
+
+
+                // mostra l'alert dialog
+                builder.show();
+
+            }
+        });
+
+
+ // -- fine codice button filter --
+
+
 
         // Inflate the layout for this fragment
         return view;
