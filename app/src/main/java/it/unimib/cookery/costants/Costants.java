a72e08006e83e2f3ac8225589f1d4651c1de5fe6
adamInajjar <a.inajjar@campus.unimib.it>
2022-01-22 12:44:29 +0100

    aggiornamento

--- app/src/main/java/it/unimib/cookery/costants/Costants.java
+++ app/src/main/java/it/unimib/cookery/costants/Costants.java
@@ -86,7 +86,7 @@ public class Costants {
 
 
 
-    public static final String API_KEY = anotherAnotherKey;
+    public static final String API_KEY = anotheranotherAnotherKey;
     public static final String BASE_URL = "https://api.spoonacular.com/";
 
     /* fine stringhe costanti per reftrofit */
